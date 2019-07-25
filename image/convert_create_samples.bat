92ce93b932bf9eab87c09fb09132b11da238a5af
jujumo <ju.jumo@gmail.com>
2019-07-25 11:02:14 +0200

    rework of convert

--- image/convert_create_samples.bat
+++ image/convert_create_samples.bat
@@ -1,3 +1,4 @@
 convert xc:black xc:red xc:yellow xc:green1 xc:cyan xc:blue xc:black +append -filter Cubic -resize 50x30! convert_rainbow.jpg
 convert pattern:gray50 convert_grey.png
 convert rose: convert_rose.png
+montage rose: rose: rose: \( pattern:gray50 -set label gray \) -background chartreuse -geometry +0+0 convert_montage.png
