e769972ca6f991e2264fc538aa73d56f89ca9de1
jujumo <ju.jumo@gmail.com>
2019-11-13 17:33:18 +0100

    wip video reader

--- coding/python/video_reader.py
+++ coding/python/video_reader.py
@@ -3,47 +3,87 @@
 import cv2
 
 
-class VideoIterator:
+class VideoInputStreamIterator:
     def __init__(self, capture):
-        self._capture = capture
+        self._stream = capture
 
     def __next__(self):
-        if not self._capture.isOpened():
-            raise StopIteration
-
-        success, frame = self._capture.read()
-        if not success or frame is None:
-            raise StopIteration
-        timestamp = self._capture.get(cv2.CAP_PROP_POS_MSEC)
-        return timestamp, frame
+        return self._stream.grab_frame()
 
 
-class VideoReader:
-    def __init__(self, video_path):
-        self._path = video_path
+class VideoInputStream:
+    def __init__(self):
+        self._capture = None
 
-    def __enter__(self):
-        self._capture = cv2.VideoCapture(self._path)
+    def open(self, video_path):
+        self._capture = cv2.VideoCapture(video_path)
         if not self._capture.isOpened():
             raise ValueError(f'unable to open video {video_path}')
         return self
 
-    def __exit__(self, type, value, traceback):
+    def close(self):
         self._capture.release()
 
+    def grab_frame(self):
+        if not self._capture.isOpened():
+            raise StopIteration
+        success, frame = self._capture.read()
+        if not success:
+            raise StopIteration
+        timestamp = self._capture.get(cv2.CAP_PROP_POS_MSEC)
+        return timestamp, frame
+
     def __len__(self):
+        assert self._capture
         # WARNING: inaccurate (based on duration and FPS)
         return int(self._capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def __iter__(self):
-        return VideoIterator(self._capture)
+        assert self._capture
+        return VideoInputStreamIterator(self)
+
+    def __enter__(self):
+        # nothing to be done
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class Video:
+    @staticmethod
+    def read(video_path):
+        video_stream = VideoInputStream()
+        video_stream.open(video_path)
+        return video_stream
 
 
 # test
 from tqdm import tqdm
 if __name__ == '__main__':
     video_path = '/tmp/frames/%05d.jpg'
-    with VideoReader(video_path) as reader:
-        for timestamp, frame in tqdm(reader):
+    with Video.read(video_path) as video_stream:
+        for timestamp, frame in tqdm(video_stream):
             cv2.imshow('frame', frame)
             cv2.waitKey(10)
+
+    video_stream = Video.read(video_path)
+    for timestamp, frame in tqdm(video_stream):
+        cv2.imshow('frame', frame)
+        cv2.waitKey(10)
+    video_stream.close()
+
+    video_stream = VideoInputStream()
+    video_stream.open(video_path)
+    for timestamp, frame in tqdm(video_stream):
+        cv2.imshow('frame', frame)
+        cv2.waitKey(10)
+    video_stream.close()
+
+    video_stream = VideoInputStream()
+    video_stream.open(video_path)
+    for idx in tqdm(range(len(video_stream))):
+        timestamp, frame = video_stream.grab_frame()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(10)
+    video_stream.close()
