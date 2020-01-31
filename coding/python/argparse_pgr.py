4a08224e7b88942f06a417137f6fc162a88dd442
jujumo <ju.jumo@gmail.com>
2020-01-31 10:57:49 +0100

    python + add_subparsers

--- coding/python/argparse_pgr.py
+++ coding/python/argparse_pgr.py
@@ -13,6 +13,14 @@ def main():
     parser.add_argument('-n', '--number', type=int, default=1, help='number')        # <5>
     parser.add_argument('-l', '--level', type=int, choices=[1, 2, 4], help='level')  # <6>
     parser.add_argument('-x', default=None, const='zip', nargs='?', help='format')   # <7>
+    subparsers = parser.add_subparsers(help='sub-command help')
+    # create the parser for the "a" command
+    parser_a = subparsers.add_parser('aaa', help='a help')
+    parser_a.add_argument('foo', help='bar help')
+    # create the parser for the "b" command
+    parser_b = subparsers.add_parser('bbb', help='b help')
+    parser_b.add_argument('--baz', choices='321', help='baz help')
+
     #################################
     # some possible usages
     usages = [
@@ -22,14 +30,16 @@ def main():
         '-i input -l 2',
         '-i input -x',
         '-i input -x tar',
+        'aaa bar',
+        'bbb --baz 1'
     ]
     # log it to file argparse_out.adoc
     from os.path import basename, dirname, join
     with open(join(dirname(__file__), 'argparse_out.adoc'), 'w') as fout:
         for usage in usages:
-            fout.write('```bash\n> python {} {}\n'.format(basename(__file__), usage))
+            fout.write('[source,bash]\n----\n> python {0} {1}\n'.format(basename(__file__), usage))
             fout.write(str(parser.parse_args(usage.split())))
-            fout.write('\n```\n')
+            fout.write('\n----\n\n')
     #################################
 
 
